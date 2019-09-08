18c9bf6780bc3cbebe394f8d9bbe7e8dee2c9f77
Jai Aravindh <jaitvsmhss@gmail.com>
2019-09-08 17:53:32 +0530

    Basic React practic, worked with simple components
    Works with simple header component which is exported.

---
+++ react_basics/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+//COMPONENTS
+import Header from './components/header'
+
+const App = () =>{
+        return (
+            <div>
+                <Header/>
+            </div>
+        )
+}
+
+
+ReactDOM.render(<App/>,document.querySelector('#root'));
+
+  

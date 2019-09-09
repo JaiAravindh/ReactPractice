aaf57e5414120424e3df66b41df223ac6c0c338e
Jai Aravindh <jaitvsmhss@gmail.com>
2019-09-09 21:07:08 +0530

    inline styles

--- react_basics/src/components/header.js
+++ react_basics/src/components/header.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React,{Component} from 'react';
 
-const getYear = () =>{
-        const newDate = new Date();
-        return newDate.getFullYear();
-}
 
-const Header = () => {
-    return <div>The Year is {
-        getYear()
-    }</div>
+class Header extends Component{
+        render(){
+               const styles = {
+                       header:{
+                               background:'#03a9f4'
+                       },
+                       logo:{
+                               color:'#fff',
+                               fontFamily:'Anton',
+                               textAlign:'center'
+                       }
+               }
+               return(
+                       <header style={styles.header}>
+                       <div style={styles.logo}>Logo</div>
+                       <input type="text"/>
+                     </header>
+                       )
+        }
 }
 
 export default Header;

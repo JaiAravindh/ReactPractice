eba10f25dbaaa8efaf3768e1003b08380c451fa9
Jai Aravindh <jaitvsmhss@gmail.com>
2019-09-11 21:38:48 +0530

    using glamor
    Install glamor : 
    npm install glamor --save

--- react_basics/src/components/news_list_item.js
+++ react_basics/src/components/news_list_item.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
+//Do the Following :
+//npm install glamor --save
+
+import {css} from 'glamor';
+
 const items = props.news.map((item) => {
+    
+     let news_item = css({
+          padding:'20px',
+          boxSizing :'border=box',
+          borderBotton : '1px solid grey',
+          ':hover'"{
+          color:'red'
+     }
+     })
+     
+     let item_grey = css({
+          background : 'lightgrey',
+     })
+     
+     
      return(
-         <div>
+          
+         <div {...news_item} {...item_grey}>
              <h3>
                  {item.title}
              </h3>

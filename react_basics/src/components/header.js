be6c5de821e2e31da911832dc2f0c27007c86d0d
Jai Aravindh <jaitvsmhss@gmail.com>
2019-09-11 08:09:23 +0530

    Update header.js

--- react_basics/src/components/header.js
+++ react_basics/src/components/header.js
@@ -2,17 +2,29 @@ import React,{Component} from 'react';
 
 
 class Header extends Component{
-        render(){
-             return(
-                       <header>
-                       <div className="logo"
-                                onClick={ ()=> console.log('I was Clicked') }
-                       >Logo
-                       </div>
-                       <input type="text"/>
-                     </header>
-                       )
-        }
+    
+    state = {
+        keywords:'Hello'
+    }
+    
+    inputChangeHandler = (event) =>{
+       // console.log(event.target.value)
+       this.setState({
+           keywords: event.target.value
+       })
+    }
+
+    render(){
+        return(
+            <header>
+                <div className="logo">Logo</div>
+                <input
+                    type='text'
+                    onChange={this.inputChangeHandler.bind(this)}
+                    />
+            </header>
+        )
+    }
 }
 
 export default Header;

601df7bd3d59fc13d9e06b0ebe8d2eb1133440ce
Jai Aravindh <jaitvsmhss@gmail.com>
2019-09-11 21:26:45 +0530

    inline styling

--- react_basics/src/components/header.js
+++ react_basics/src/components/header.js
@@ -5,29 +5,32 @@ class Header extends Component{
     
     state = {
         keywords:'Hello'
+        active : false;
     }
     
     inputChangeHandler = (event) =>{
        // console.log(event.target.value)
-       this.setState({
-           keywords: event.target.value
+        const value = event.target.value === '' ? false : true; 
+        this.setState({
+           keywords: event.target.value,
+            active : value
        })
     }
 
     render(){
-        const style = {
-            background:'red'
-        }
+       // const style = {
+       //     background:'red'
+       // }
         
-        if(this.state.keywords !== 'blue'){
-            style.background = 'blue';
-        }
-        else{
-            style.background = 'red';
-        }
+        //if(this.state.keywords !== 'blue'){
+          //  style.background = 'blue';
+       // }
+       //else{
+       //    style.background = 'red';
+       //}
         
         return(
-            <header style={{background:'red'}}>
+            <header style={{background:'${this.state.active ? 'red' : 'blue'}'}}>
                 <div className="logo">Logo</div>
                 <input
                     type='text'

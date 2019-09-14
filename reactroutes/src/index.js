d8abc92bad6e196c3de7bf7ee5babfc28f1302d5
Jai Aravindh <jaitvsmhss@gmail.com>
2019-09-14 13:23:39 +0530

    MemoryRouter

--- reactroutes/src/index.js
+++ reactroutes/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Route} from 'react-router-dom';
+import {MemoryRouter, Route} from 'react-router-dom';
 
 //COMPONENTS
 import Home from './components/home';
@@ -10,7 +10,7 @@ import profiles from './components/profiles';
 
 const App = () => {
     return(
-        <HashRouter>
+        <MemoryRouter>
     <div>
             <Link to="/">Home</Link>
             <Link to="/posts">posts</Link>
@@ -26,7 +26,7 @@ const App = () => {
     </div>
             
             
-        </HashRouter>
+        </MemoryRouter>
     )
 }
 
